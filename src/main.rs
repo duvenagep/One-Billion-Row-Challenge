d3c3444cf93d3e872d95cea4931b195ea031bb35
Paul Duvenage <duvenagep@gmail.com>
2024-07-18 09:47:08 +0200

    feat(naive): Naive Solution unsorted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /// List of Implementations and optimisations
 ///
-/// 1. Naive ->
+/// 1. Naive -> 223.3567s
 mod naive;
 
 use naive::{naive, read_lines};
@@ -9,9 +9,14 @@ use std::time::Instant;
 fn main() {
     println!("Welcome to the 1BRC");
     let now = Instant::now();
-    let p = "data/test.txt";
+    let p = "data/measurements.txt";
     let hm = naive(p);
-    println!("{:?}", hm);
+    let mut v = hm.into_iter().collect::<Vec<_>>();
+    // v.sort_unstable_by_key(|p| p.0);
+    for (name, r) in v {
+        println!("{name}: {:.1}/{:.1}/{:.1}", r.1, r.2, r.3);
+    }
+    // println!("{:?}", hm);
     let elapsed = now.elapsed();
     println!("Elapsed: {elapsed:.4?}");
 }
--- src/naive.rs
+++ src/naive.rs
@@ -1,3 +1,4 @@
+/// Naive implementations akin to a pythonic script
 use std::{collections::HashMap, fs::read_to_string};
 
 pub fn read_lines(filename: &str) -> Vec<String> {
@@ -6,19 +7,26 @@ pub fn read_lines(filename: &str) -> Vec<String> {
     for line in read_to_string(filename).unwrap().lines() {
         result.push(line.to_string())
     }
-
     result
 }
 
-pub fn naive(path: &str) -> HashMap<String, f32> {
-    let mut hm: HashMap<String, f32> = HashMap::new();
+pub fn naive(path: &str) -> HashMap<String, (u32, f32, f32, f32)> {
+    let mut hm: HashMap<String, (u32, f32, f32, f32)> = HashMap::new();
     let lines = read_lines(path);
     for line in lines {
         let (station, temp) = line
             .split_once(";")
             .map(|(s, t)| (s.to_string(), t.parse::<f32>().unwrap()))
             .unwrap();
-        hm.insert(station, temp);
+
+        hm.entry(station)
+            .and_modify(|(c, s, m, mx)| {
+                *c += 1;
+                *s += &temp;
+                *m = m.min(temp);
+                *mx = mx.max(temp);
+            })
+            .or_insert((1, temp, temp, temp));
     }
     hm
 }
